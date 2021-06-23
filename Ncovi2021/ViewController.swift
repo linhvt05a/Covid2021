00538b8b231d10f687589d48765df0ebc8d2b989
iMac <hoanglinh6794@gmail.com>
2021-06-23 11:23:48 +0700

    Initial Commit

---
+++ Ncovi2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ncovi2021
+//
+//  Created by iMac on 23/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
